56732152ab50db7e12d83980954c04b55b8cee10
Edgar Bello <edgar.bello@gorillalogic.com>
2019-07-18 12:51:21 -0600

    CRUD!!!

--- agenda.js
+++ agenda.js
@@ -1,1 +1,78 @@
-alert("test");
+var dom = document;
+var names = ["Ed", "Andres", "Maria", "Sergio", "Dany"];
+
+function showNames() {
+    var ul = dom.getElementById("nameList");
+    ul.innerHTML = "";
+    names.forEach(function(name) {
+        var li = dom.createElement('li');
+        ul.appendChild(li);
+
+        li.innerHTML += name;
+    });
+}
+
+function addNewName() {
+    let newName = dom.getElementById("nameInput").value;
+    names.push(newName);
+    console.log(names);
+    dom.getElementById("actionResult").innerHTML = "Add New Name Success: " + newName;
+};
+
+function updateName() {
+    document.getElementById("fixNameSection").style.display = "";
+};
+
+
+function fixName() {
+    let editName = dom.getElementById("nameInput").value;
+    let fixedName = dom.getElementById("fixNameInput").value;
+    if (updateNameArray(editName, fixedName)) {
+        dom.getElementById("actionResult").innerHTML = "Fix Name Success: " + editName + " -> " + fixedName;
+    } else {
+        dom.getElementById("actionResult").innerHTML = "Name Not Found";
+    }
+    document.getElementById("fixNameSection").style.display = "none";
+};
+
+function removeName() {
+    let removeName = dom.getElementById("nameInput").value;
+    if (removeNameArray(removeName)) {
+        dom.getElementById("actionResult").innerHTML = "Remove Name Success: " + removeName + " Removed";
+    } else {
+        dom.getElementById("actionResult").innerHTML = "Remove Name Not Found";
+    }
+};
+
+function updateNameArray(editName, fixedName) {
+    var changed = false;
+    for (i = 0; i < names.length; i++) {
+        if (names[i] === editName) {
+            names[i] = fixedName;
+            changed = true;
+        }
+    }
+    console.log(names);
+    return changed;
+}
+
+function removeNameArray(removeName) {
+    var removed = false;
+    for (i = 0; i < names.length; i++) {
+        if (names[i] === removeName) {
+            names.splice(i, 1);
+            removed = true;
+        }
+    }
+    console.log(names);
+    return removed;
+}
+
+function clearNames() {
+    var ul = dom.getElementById("nameList");
+    ul.innerHTML = "";
+    dom.getElementById("actionResult").innerHTML = "Clear All Names Success!";
+};
+
+console.log(names);
+document.getElementById("fixNameSection").style.display = "none";
